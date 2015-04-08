578569806f835a7d355dea7577bce2e995308786
jahanarun <jahanarun.j@live.com>
2015-04-08 12:15:41 +0530

    Added Setup and DbUpdateDelivery projects

---
+++ master/DbUpdateDelivery/Module.cs
@@ -0,0 +1,13 @@
+﻿namespace DbUpdateDelivery
+{
+    public class Module
+    {
+
+        public const string ConnectionString =
+            @"Data Source=DEV-PC\DEXLAB_SQLSERVER; Initial Catalog=DrugHouse; User Id=dev; Password=dev1234;";
+
+        public const string SqlText = @"
+insert into Drug values ('Tester', 2,'','Drug')
+";
+    }
+}
---
+++ master/DbUpdateDelivery/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.SqlClient;
+
+namespace DbUpdateDelivery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            using (var conn = new SqlConnection(Module.ConnectionString))
+            {
+                conn.Open();
+                var cmd = new SqlCommand(Module.SqlText, conn);
+                cmd.ExecuteNonQuery();  
+                conn.Close();
+                
+            }
+        }
+    }
+}
