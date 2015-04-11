c64491155e94d77ef9ef696b7f9d5c08ec0c3372
jahanarun <jahanarun.j@live.com>
2015-04-11 18:28:19 +0530

    Introduced ObservableCommand and ExecuteCommand

---
+++ master/Dexuse/ICommand/ObservableCommand.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Dexuse.ICommand
+{
+    public abstract class ObservableCommand : System.Windows.Input.ICommand
+    {
+        ///<summary>
+        /// The event fired when a command is executed;
+        ///</summary>
+        public event EventHandler CommandExecuted = delegate { };
+        /// <summary>
+        /// Defines the method to be called when the command is invoked.
+        /// </summary>
+        /// <param name="parameter">Data used by the command.  If the command does not require data to be passed, this object can be set to null. </param>
+        public void Execute(object parameter)
+        {
+            ExecuteCommand(parameter);
+            CommandExecuted(this, EventArgs.Empty);
+        }
+        protected abstract void ExecuteCommand(object parameter);
+        /// <summary>
+        /// Defines the method that determines whether the command can execute in its current state. 
+        /// </summary>
+        /// <returns>
+        /// true if this command can be executed; otherwise, false.
+        /// </returns>
+        /// <param name="parameter">Data used by the command.  If the command does not require data to be passed, this object can be set to null.
+        ///                 </param>
+        public virtual bool CanExecute(object parameter)
+        {
+            return true;
+        }
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged(this, EventArgs.Empty);
+        }
+
+        /// <summary>
+        /// Occurs when changes occur that affect whether or not the command should execute.
+        /// </summary>
+        public event EventHandler CanExecuteChanged = delegate { };
+    }
+}
---
+++ master/DrugHouse.ViewModel/Common/ExecutedCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Dexuse.ICommand;
+
+namespace DrugHouse.ViewModel.Common
+{
+    internal class ExecutedCommand : ObservableCommand
+    {
+        private readonly Action Action;
+        private readonly Func<bool> Predicate;
+
+        public ExecutedCommand(Action action, Func<bool> canExecute )
+        {
+            Action = action;
+            Predicate = canExecute;
+
+        }
+        protected override void ExecuteCommand(object parameter)
+        {
+            Action.Invoke();
+        }
+
+        public override bool CanExecute(object parameter)
+        {
+            return Predicate.Invoke();
+        }
+    }
+}
